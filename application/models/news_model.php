3815f19cdfdac1345220d263670f06bfa73f1c9a
Siwaphol Boonpan <nong-siwa@hotmail.com>
2016-06-04 17:20:40 +0700

    Edit news_model

--- application/models/news_model.php
+++ application/models/news_model.php
@@ -13,7 +13,7 @@ class News_model extends CI_Model {
 			$query = $this->db->get('news');
 			return $query->result_array();
 		}
-
+		
 		$query = $this->db->get_where('news', array('slug' => $slug));
 		return $query->row_array();
 	}
