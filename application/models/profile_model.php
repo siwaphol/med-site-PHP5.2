37e10ffeba3cc031c57c876b48ffee78a08a0322
Siwaphol Boonpan <nong-siwa@hotmail.com>
2016-06-11 13:55:59 +0700

    Add config/* ignore

--- application/models/profile_model.php
+++ application/models/profile_model.php
@@ -9,20 +9,20 @@ class Profile_model extends CI_Model {
 	public function get_profile($id=FALSE)
 	{
         if($id===FALSE){
-            $query = $this->db->get('user_profiles');
+            $query = $this->db->get('staffs');
             return $query->result_array();
         }
-        $query = $this->db->get_where('user_profiles',array('id'=>$id));
+        $query = $this->db->get_where('staffs',array('id'=>$id));
         return $query->row_array();
     }
 
     public function record_count() {
-        return $this->db->count_all("user_profiles");
+        return $this->db->count_all("staffs");
     }
 
     public function fetch_profiles($limit, $start) {
         $this->db->limit($limit, $start);
-        $query = $this->db->get("user_profiles");
+        $query = $this->db->get("staffs");
 
         if ($query->num_rows() > 0) {
             foreach ($query->result() as $row) {
